6f1917bcb079913cf2fddfcf45b1cf8af3ccc588
Paris Tsantarliotis <paris.tsantarliotis@gmail.com>
2020-05-24 03:12:27 +0300

    Applied the changes required for the "Microservices at Scale using AWS & Kubernetes" lesson of Udacity CloudDevops Nanodegree.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="ptsantar/house_price_prediction_udacity_dev_ops"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment devops4 --image=$dockerpath 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/devops4 8080:80 
--- upload_docker.sh
+++ upload_docker.sh
@@ -4,12 +4,19 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+# Create dockerpath and find the latest image ID 
+dockerpath="ptsantar/house_price_prediction_udacity_dev_ops"
+imageID=$(docker image ls | grep housepriceprediction | grep latest | awk '{print $3}')
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Authentication"
+docker login --username ptsantar
+
+echo "Tagging image $imageID to $dockerpath"
+docker tag $imageID $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+echo "Pushing image"
+docker push $dockerpath
